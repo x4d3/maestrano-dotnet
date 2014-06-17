944f9cb234b33ced0e390f93992a7e2f64c63483
alachaum <arnaud.lachaume@maestrano.com>
2014-06-16 20:51:50 -0700

    placeholders for SAML

---
+++ src/Maestrano.Tests/Saml/SettingsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Maestrano.Tests.Saml
+{
+    [TestClass]
+    public class SettingsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

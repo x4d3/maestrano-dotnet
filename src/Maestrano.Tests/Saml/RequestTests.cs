88605ab5051015487cb59ca014f2721a5033631b
alachaum <arnaud.lachaume@maestrano.com>
2014-06-17 00:06:43 -0700

    add tests and fixed params

--- src/Maestrano.Tests/Saml/RequestTests.cs
+++ src/Maestrano.Tests/Saml/RequestTests.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Xml;
+using System.Web;
+using Maestrano.Saml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Maestrano.Tests.Saml
@@ -6,9 +11,121 @@ namespace Maestrano.Tests.Saml
     [TestClass]
     public class RequestTests
     {
+        
+
         [TestMethod]
-        public void TestMethod1()
+        public void GetRequest_ItReturnsTheRightBased64EncodedXmlRequest()
         {
+            Maestrano.Environment = "production";
+            Maestrano.App.Host = "https://mysuperapp.com";
+            Maestrano.Api.Id = "app-1";
+
+            Request req = new Request();
+
+            Assert.AreEqual(XmlRequestBase64Encoded(req.Id, req.IssueInstant), req.GetXmlBase64Request());
+        }
+
+        [TestMethod]
+        public void RedirectUrl_WithNoParameters_ItReturnsTheRightUrl()
+        {
+            Maestrano.Environment = "production";
+            Maestrano.App.Host = "https://mysuperapp.com";
+            Maestrano.Api.Id = "app-1";
+
+            // Build parameters
+            NameValueCollection parameters = new NameValueCollection();
+            parameters.Add("group_uid","someparamvalue");
+            parameters.Add("someotherparam","someothervalue");
+
+            // Build request
+            Request req = new Request(parameters);
+
+            // Build expected url
+            string expectedUrl = Maestrano.Sso.IdpUrl();
+            expectedUrl += "?SAMLRequest=";
+            expectedUrl += HttpUtility.UrlEncode(XmlRequestBase64Encoded(req.Id, req.IssueInstant));
+            expectedUrl += "&group_uid=someparamvalue";
+            expectedUrl += "&someotherparam=someothervalue";
+
+            Assert.AreEqual(expectedUrl, req.RedirectUrl());
+        }
+
+        [TestMethod]
+        public void RedirectUrl_WithParameters_ItReturnsTheRightUrl()
+        {
+            Maestrano.Environment = "production";
+            Maestrano.App.Host = "https://mysuperapp.com";
+            Maestrano.Api.Id = "app-1";
+
+            // Build request
+            Request req = new Request();
+
+            // Build expected url
+            string expectedUrl = Maestrano.Sso.IdpUrl();
+            expectedUrl += "?SAMLRequest=";
+            expectedUrl += HttpUtility.UrlEncode(XmlRequestBase64Encoded(req.Id, req.IssueInstant));
+
+            Assert.AreEqual(expectedUrl, req.RedirectUrl());
+        }
+
+
+        /// <summary>
+        /// Simulate a Base64 encoded XML Request
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="issueInstant"></param>
+        /// <param name="consumeUrl"></param>
+        /// <param name="issuer"></param>
+        /// <param name="nameIdFormat"></param>
+        /// <returns></returns>
+        public string XmlRequestBase64Encoded(string id, string issueInstant, string consumeUrl = null, string issuer = null, string nameIdFormat = null)
+        {
+            // Default values
+            if (string.IsNullOrEmpty(consumeUrl))
+                consumeUrl = Maestrano.Sso.ConsumeUrl();
+            if (string.IsNullOrEmpty(nameIdFormat))
+                nameIdFormat = Maestrano.Sso.NameIdFormat;
+            if (string.IsNullOrEmpty(issuer))
+                issuer = Maestrano.Api.Id;
+
+            using (StringWriter sw = new StringWriter())
+            {
+                XmlWriterSettings xws = new XmlWriterSettings();
+                xws.OmitXmlDeclaration = true;
+
+                using (XmlWriter xw = XmlWriter.Create(sw, xws))
+                {
+                    xw.WriteStartElement("samlp", "AuthnRequest", "urn:oasis:names:tc:SAML:2.0:protocol");
+                    xw.WriteAttributeString("ID", id);
+                    xw.WriteAttributeString("Version", "2.0");
+                    xw.WriteAttributeString("IssueInstant", issueInstant);
+                    xw.WriteAttributeString("ProtocolBinding", "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST");
+                    xw.WriteAttributeString("AssertionConsumerServiceURL", consumeUrl);
+
+                    xw.WriteStartElement("saml", "Issuer", "urn:oasis:names:tc:SAML:2.0:assertion");
+                    xw.WriteString(issuer);
+                    xw.WriteEndElement();
+
+                    xw.WriteStartElement("samlp", "NameIDPolicy", "urn:oasis:names:tc:SAML:2.0:protocol");
+                    xw.WriteAttributeString("Format", nameIdFormat);
+                    xw.WriteAttributeString("AllowCreate", "true");
+                    xw.WriteEndElement();
+
+                    xw.WriteStartElement("samlp", "RequestedAuthnContext", "urn:oasis:names:tc:SAML:2.0:protocol");
+                    xw.WriteAttributeString("Comparison", "exact");
+                    xw.WriteEndElement();
+
+                    xw.WriteStartElement("saml", "AuthnContextClassRef", "urn:oasis:names:tc:SAML:2.0:assertion");
+                    xw.WriteString("urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport");
+                    xw.WriteEndElement();
+
+                    xw.WriteEndElement();
+                }
+
+                // Encode in base64
+                byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(sw.ToString());
+                return System.Convert.ToBase64String(toEncodeAsBytes);
+            }
         }
     }
 }
--- src/Maestrano/Saml/Request.cs
+++ src/Maestrano/Saml/Request.cs
@@ -10,12 +10,13 @@ using System.Web;
 
 namespace Maestrano.Saml
 {
-    class Request
+    public class Request
     {
-        public string id;
-        private string issue_instant;
-        private Settings _settings;
-        private NameValueCollection _parameters;
+        public string Id {get; private set;}
+        public string IssueInstant { get; private set; }
+
+        public Settings settings { get; private set; }
+        public NameValueCollection parameters { get; private set; }
 
         public enum RequestFormat
         {
@@ -28,11 +29,11 @@ namespace Maestrano.Saml
         /// <param name="parameters">The request GET parameters typically obtained via HttpRequest.Params</param>
         public Request(NameValueCollection parameters = null)
         {
-            this._parameters = parameters;
-            this._settings = Maestrano.Sso.SamlSettings();
+            this.parameters = parameters;
+            this.settings = Maestrano.Sso.SamlSettings();
 
-            id = "_" + Guid.NewGuid().ToString();
-            issue_instant = DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
+            Id = "_" + Guid.NewGuid().ToString();
+            IssueInstant = DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
         }
 
 
@@ -40,7 +41,7 @@ namespace Maestrano.Saml
         /// Return the SAML XML request, Base64 encoded
         /// </summary>
         /// <returns></returns>
-        public string GetRequest()
+        public string GetXmlBase64Request()
         {
             using (StringWriter sw = new StringWriter())
             {
@@ -50,18 +51,18 @@ namespace Maestrano.Saml
                 using (XmlWriter xw = XmlWriter.Create(sw, xws))
                 {
                     xw.WriteStartElement("samlp", "AuthnRequest", "urn:oasis:names:tc:SAML:2.0:protocol");
-                    xw.WriteAttributeString("ID", id);
+                    xw.WriteAttributeString("ID", Id);
                     xw.WriteAttributeString("Version", "2.0");
-                    xw.WriteAttributeString("IssueInstant", issue_instant);
+                    xw.WriteAttributeString("IssueInstant", IssueInstant);
                     xw.WriteAttributeString("ProtocolBinding", "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST");
-                    xw.WriteAttributeString("AssertionConsumerServiceURL", _settings.AssertionConsumerServiceUrl);
+                    xw.WriteAttributeString("AssertionConsumerServiceURL", settings.AssertionConsumerServiceUrl);
 
                     xw.WriteStartElement("saml", "Issuer", "urn:oasis:names:tc:SAML:2.0:assertion");
-                    xw.WriteString(_settings.Issuer);
+                    xw.WriteString(settings.Issuer);
                     xw.WriteEndElement();
 
                     xw.WriteStartElement("samlp", "NameIDPolicy", "urn:oasis:names:tc:SAML:2.0:protocol");
-                    xw.WriteAttributeString("Format", _settings.NameIdentifierFormat);
+                    xw.WriteAttributeString("Format", settings.NameIdentifierFormat);
                     xw.WriteAttributeString("AllowCreate", "true");
                     xw.WriteEndElement();
 
@@ -91,14 +92,14 @@ namespace Maestrano.Saml
         public string RedirectUrl()
         {
             // Build the base url
-            string url = _settings.IdpSsoTargetUrl;
+            string url = settings.IdpSsoTargetUrl;
             url += "?SAMLRequest=";
-            url += HttpUtility.UrlEncode(this.GetRequest());
+            url += HttpUtility.UrlEncode(this.GetXmlBase64Request());
 
             // Add query parameters
-            if (_parameters.HasKeys())
-                foreach (String k in _parameters.AllKeys)
-                    url += "&" + k + HttpUtility.UrlEncode(_parameters[k]);
+            if (parameters != null)
+                foreach (String k in parameters.AllKeys)
+                    url += "&" + k + "=" + HttpUtility.UrlEncode(parameters[k]);
 
             return url;
         }

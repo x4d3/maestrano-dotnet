09c445ca909ae84090c2d100936840e544536f59
alachaum <arnaud.lachaume@maestrano.com>
2014-06-26 10:51:25 -0700

    Added crud methods

--- src/Maestrano.Tests/Account/RecurringBillTest.cs
+++ src/Maestrano.Tests/Account/RecurringBillTest.cs
@@ -8,16 +8,51 @@ namespace Maestrano.Tests.Account
     [TestClass]
     public class RecurringBillTest
     {
-        //private MnoClient _client;
-
         public RecurringBillTest()
-		{
-			//_client = new MnoClient();
-		}
+        {
+            Maestrano.Environment = "test";
+            Maestrano.Api.Id = "app-1";
+            Maestrano.Api.Key = "gfcmbu8269wyi0hjazk4t7o1sndpvrqxl53e1";
+        }
 
         [TestMethod]
-        public void Create_ItShouldBeSuccessfull()
+        public void All_ItShouldReturnTheListOfBills()
         {
+            Console.WriteLine(Maestrano.Api.Id);
+            var list = RecurringBill.All();
+            Assert.AreEqual("rbill-1", list[0].Id);
+            Assert.AreEqual("rbill-2", list[1].Id);
+        }
+
+        [TestMethod]
+        public void Retrieve_ItShouldReturnASingleBill()
+        {
+            Console.WriteLine(Maestrano.Api.Id);
+            var obj = RecurringBill.Retrieve("rbill-1");
+            Assert.AreEqual("rbill-1", obj.Id);
+        }
+
+        [TestMethod]
+        public void Create_ItShouldCreateABill()
+        {
+            var obj = RecurringBill.Create(
+                groupId: "cld-3",
+                priceCents: 1500,
+                description: "Some Bill"
+                );
+            Assert.IsNotNull(obj.Id);
+        }
+
+        [TestMethod]
+        public void Cancel_ItShouldCancelABill()
+        {
+            var obj = RecurringBill.Create(
+                groupId: "cld-3",
+                priceCents: 1500,
+                description: "Some Bill"
+                );
+            obj.Cancel();
+            Assert.AreEqual("cancelled", obj.Status);
         }
     }
 }
--- src/Maestrano/Account/RecurringBill.cs
+++ src/Maestrano/Account/RecurringBill.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Maestrano.Api;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,76 @@ namespace Maestrano.Account
 {
     public class RecurringBill
     {
+        public string Id { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public String Status { get; set; }
+        public decimal? Units { get; set; }
+        public String Period { get; set; }
+        public Int16? Frequency { get; set; }
+        public Int16? Cycles { get; set; }
+        public DateTime? StartDate { get; set; }
+
+        // Mandatory for creation
+        public string GroupId { get; set; }
+        public Int32 PriceCents { get; set; }
+        public string Currency { get; set; }
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The Resource name
+        /// </summary>
+        /// <returns></returns>
+        public static string IndexPath()
+        {
+            return "account/recurring_bills";
+        }
+
+        /// <summary>
+        /// The Single Resource name
+        /// </summary>
+        /// <returns></returns>
+        public static string ResourcePath()
+        {
+            return IndexPath() + "/{id}";
+        }
+
+        public static List<RecurringBill> All(NameValueCollection filters = null)
+        {
+            return MnoClient.All<RecurringBill>(IndexPath(), filters);
+        }
+
+        public static RecurringBill Retrieve(string billId)
+        {
+            return MnoClient.Retrieve<RecurringBill>(ResourcePath(), billId);
+        }
+
+        public static RecurringBill Create(String groupId, Int32 priceCents, String description, String currency = "AUD", Int16? frequency = null, Int16? cycles = null, DateTime? startDate = null)
+        {
+            var att = new NameValueCollection();
+            att.Add("groupId", groupId);
+            att.Add("priceCents", priceCents.ToString());
+            att.Add("description", description);
+            att.Add("currency", currency);
+            if (frequency.HasValue)
+                att.Add("frequency", frequency.Value.ToString());
+            if (cycles.HasValue)
+                att.Add("cycles", cycles.Value.ToString());
+            if (startDate.HasValue)
+                att.Add("startDate", startDate.Value.ToString("s"));
+
+            return MnoClient.Create<RecurringBill>(IndexPath(), att);
+        }
+
+        public Boolean Cancel()
+        {
+            if (Id != null)
+            {
+                RecurringBill respBill = MnoClient.Delete<RecurringBill>(ResourcePath(), Id);
+                Status = respBill.Status;
+                return (Status.Equals("cancelled"));
+            }
+
+            return false;
+        }
     }
 }

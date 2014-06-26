cff567d67b4644bf99d2ad0822d40563c3262c3d
alachaum <arnaud.lachaume@maestrano.com>
2014-06-26 06:11:01 -0700

    fixed typos

--- src/Maestrano/Api/MnoArray.cs
+++ src/Maestrano/Api/MnoArray.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace Maestrano.Api
 {
-    class MnoArray : IEnumerable<JsonObject>
+    public class MnoArray : IEnumerable<JsonObject>
 	{
 		private JsonObject _internalJson;
 
--- src/Maestrano/Api/MnoClient.cs
+++ src/Maestrano/Api/MnoClient.cs
@@ -7,87 +7,84 @@ using RestSharp;
 
 namespace Maestrano.Api
 {
-    class MnoClient
+    public partial class MnoClient
     {
-        public partial class MnoClient
+        public string ApiVersion { get; set; }
+        public string ApiEndpoint { get; set; }
+        public string ApiKey { get; private set; }
+
+        private RestClient _client;
+
+        public MnoClient(string apiKey)
         {
-            public string ApiVersion { get; set; }
-            public string ApiEndpoint { get; set; }
-            public string ApiKey { get; private set; }
+            ApiVersion = "v1";
+            ApiEndpoint = "https://maestrano.com/api";
+            ApiKey = apiKey;
 
-            private RestClient _client;
+            // silverlight friendly way to get current version
+            var assembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = new AssemblyName(assembly.FullName);
+            var version = assemblyName.Version;
 
-            public MnoClient(string apiKey)
+            _client = new RestClient();
+            _client.UserAgent = "maestrano-dotnet/" + version;
+            _client.Authenticator = new MnoAuthenticator(apiKey);
+            _client.BaseUrl = String.Format("{0}{1}", ApiEndpoint, ApiVersion);
+        }
+
+        /// <summary>
+        /// Execute a manual REST request
+        /// </summary>
+        /// <typeparam name="T">The type of object to create and populate with the returned data.</typeparam>
+        /// <param name="request">The RestRequest to execute (will use client credentials)</param>
+        public MnoObject ExecuteObject(RestRequest request)
+        {
+            request.OnBeforeDeserialization = (resp) =>
             {
-                ApiVersion = "v1";
-                ApiEndpoint = "https://maestrano.com/api";
-                ApiKey = apiKey;
+                // for individual resources when there's an error to make
+                // sure that RestException props are populated
+                if (((int)resp.StatusCode) >= 400)
+                    request.RootElement = "";
+            };
 
-                // silverlight friendly way to get current version
-                var assembly = Assembly.GetExecutingAssembly();
-                AssemblyName assemblyName = new AssemblyName(assembly.FullName);
-                var version = assemblyName.Version;
+            var response = _client.Execute(request);
+            var json = Deserialize(response.Content);
+            var obj = new MnoObject();
+            obj.SetModel(json);
 
-                _client = new RestClient();
-                _client.UserAgent = "maestrano-dotnet/" + version;
-                _client.Authenticator = new MnoAuthenticator(apiKey);
-                _client.BaseUrl = String.Format("{0}{1}", ApiEndpoint, ApiVersion);
-            }
+            return obj;
+        }
 
-            /// <summary>
-            /// Execute a manual REST request
-            /// </summary>
-            /// <typeparam name="T">The type of object to create and populate with the returned data.</typeparam>
-            /// <param name="request">The RestRequest to execute (will use client credentials)</param>
-            public MnoObject ExecuteObject(RestRequest request)
+        /// <summary>
+        /// Execute a manual REST request
+        /// </summary>
+        /// <typeparam name="T">The type of object to create and populate with the returned data.</typeparam>
+        /// <param name="request">The RestRequest to execute (will use client credentials)</param>
+        public MnoArray ExecuteArray(RestRequest request)
+        {
+            request.OnBeforeDeserialization = (resp) =>
             {
-                request.OnBeforeDeserialization = (resp) =>
-                {
-                    // for individual resources when there's an error to make
-                    // sure that RestException props are populated
-                    if (((int)resp.StatusCode) >= 400)
-                        request.RootElement = "";
-                };
+                // for individual resources when there's an error to make
+                // sure that RestException props are populated
+                if (((int)resp.StatusCode) >= 400)
+                    request.RootElement = "";
+            };
 
-                var response = _client.Execute(request);
-                var json = Deserialize(response.Content);
-                var obj = new MnoObject();
-                obj.SetModel(json);
+            var response = _client.Execute(request);
+            var json = Deserialize(response.Content);
+            var obj = new MnoArray();
+            obj.SetModel(json);
 
-                return obj;
-            }
+            return obj;
+        }
 
-            /// <summary>
-            /// Execute a manual REST request
-            /// </summary>
-            /// <typeparam name="T">The type of object to create and populate with the returned data.</typeparam>
-            /// <param name="request">The RestRequest to execute (will use client credentials)</param>
-            public MnoArray ExecuteArray(RestRequest request)
-            {
-                request.OnBeforeDeserialization = (resp) =>
-                {
-                    // for individual resources when there's an error to make
-                    // sure that RestException props are populated
-                    if (((int)resp.StatusCode) >= 400)
-                        request.RootElement = "";
-                };
+        private IDictionary<string, object> Deserialize(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                return null;
 
-                var response = _client.Execute(request);
-                var json = Deserialize(response.Content);
-                var obj = new MnoArray();
-                obj.SetModel(json);
-
-                return obj;
-            }
-
-            private IDictionary<string, object> Deserialize(string input)
-            {
-                if (String.IsNullOrEmpty(input))
-                    return null;
-
-                var serializer = new JavaScriptSerializer();
-                return serializer.Deserialize<IDictionary<string, object>>(input);
-            }
+            var serializer = new JavaScriptSerializer();
+            return serializer.Deserialize<IDictionary<string, object>>(input);
         }
     }
 }

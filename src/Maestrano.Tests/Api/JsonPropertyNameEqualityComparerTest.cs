3c05b9a448ce9a03aa6ccffe50de9c08bdcea463
alachaum <arnaud.lachaume@maestrano.com>
2014-06-26 06:10:47 -0700

    fixed typos

---
+++ src/Maestrano.Tests/Api/JsonPropertyNameEqualityComparerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using Maestrano.Api;
+
+namespace Maestrano.Tests.Api
+{
+    public class JsonPropertyNameEqualityComparerTest
+    {
+        [Fact]
+        public void Straight()
+        {
+            var eq = new JsonPropertyNameEqualityComparer();
+            var s1 = "Test";
+            var s2 = "Test";
+
+            Assert.True(eq.Equals(s1, s2));
+        }
+
+        [Fact]
+        public void Different_Case()
+        {
+            var eq = new JsonPropertyNameEqualityComparer();
+            var s1 = "Test";
+            var s2 = "test";
+
+            Assert.True(eq.Equals(s1, s2));
+        }
+
+        [Fact]
+        public void Different_Underscore()
+        {
+            var eq = new JsonPropertyNameEqualityComparer();
+            var s1 = "TestProperty";
+            var s2 = "Test_Property";
+
+            Assert.True(eq.Equals(s1, s2));
+        }
+
+        [Fact]
+        public void Different_Underscore_And_Case()
+        {
+            var eq = new JsonPropertyNameEqualityComparer();
+            var s1 = "TestProperty";
+            var s2 = "test_property";
+
+            Assert.True(eq.Equals(s1, s2));
+        }
+    }
+}

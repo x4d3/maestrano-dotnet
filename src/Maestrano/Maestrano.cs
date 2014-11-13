aee49409ff1abf1306ca62442b1240c831ce748f
alachaum <arnaud.lachaume@maestrano.com>
2014-11-12 23:42:38 -0800

    Added support for net40

--- src/Maestrano/Maestrano.cs
+++ src/Maestrano/Maestrano.cs
@@ -11,7 +11,7 @@ namespace Maestrano
     public static class MnoHelper
     {
         // VERSION
-        public static string Version { get { return "0.7.0"; } }
+        public static string Version { get { return "0.8.0"; } }
 
         
         public static Configuration.Sso Sso { get; private set; }
--- src/Maestrano/Properties/Assembly.cs
+++ src/Maestrano/Properties/Assembly.cs
@@ -12,6 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -30,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]

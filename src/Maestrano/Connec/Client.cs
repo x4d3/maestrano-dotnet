213f65258d9047abdac9dbea940b76119a8990bb
alachaum <arnaud.lachaume@maestrano.com>
2015-08-04 01:17:27 -0700

    add preset scoping on Connec Client

--- src/Maestrano/Connec/Client.cs
+++ src/Maestrano/Connec/Client.cs
@@ -13,37 +13,78 @@ namespace Maestrano.Connec
 {
     public class Client
     {
-        public string ConnecBasePath { get; set; }
-        public string ConnecScopedPath { get; set; }
-        public string ConnecHost { get; set; }
-        public string ApiId { get; set; }
-        public string ApiKey { get; set; }
-
-        public RestClient _client;
+        private string groupId;
+        private string presetName;
+        private RestClient _client;
 
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="groupId">The customer group id</param>
-        public Client (string groupId)
+        public Client (string groupId = null)
         {
-            ConnecBasePath = MnoHelper.Connec.BasePath;
-            ConnecScopedPath = ConnecBasePath + "/" + groupId;
-            ConnecHost = MnoHelper.Connec.Host;
-            ApiKey = MnoHelper.Api.Key;
-            ApiId = MnoHelper.Api.Id;
+            this.New(groupId);
+        }
 
-            // silverlight friendly way to get current version
-            var assembly = Assembly.GetExecutingAssembly();
-            AssemblyName assemblyName = new AssemblyName(assembly.FullName);
-            var version = assemblyName.Version;
+        /// <summary>
+        /// Return a Client for a specific preset
+        /// New scoped clients can be initialized with: Client.With("preset").New("group-id")
+        /// </summary>
+        /// <param name="presetName"></param>
+        /// <returns></returns>
+        public static Client With(string presetName = "maestrano") {
+            Client prepClient = new Client();
+            prepClient.presetName = presetName;
+            return prepClient;
+        }
 
-            _client = new RestClient();
-            _client.UserAgent = "maestrano-dotnet/" + version;
-            _client.Authenticator = new HttpBasicAuthenticator(ApiId, ApiKey);
-            _client.BaseUrl = String.Format("{0}{1}", ConnecHost, ConnecScopedPath);
-            _client.AddDefaultHeader("Accept", "application/vnd.api+json");
-            _client.AddDefaultHeader("Content-Type", "application/vnd.api+json");
+        /// <summary>
+        /// Reinitialize a Client with a new group id, but without modifying
+        /// any existing configuration preset
+        /// </summary>
+        /// <param name="groupId"></param>
+        /// <returns></returns>
+        public Client New(string groupId)
+        {
+            this.groupId = groupId;
+            this._client = null;
+            if (presetName == null)
+            {
+                presetName = "maestrano";
+            }
+            return this;
+        }
+
+        private RestClient InternalClient()
+        {
+            if (_client == null)
+            {
+                var connecBasePath = MnoHelper.With(presetName).Connec.BasePath;
+                var connecScopedPath = connecBasePath + "/" + groupId;
+
+                // silverlight friendly way to get current version
+                var assembly = Assembly.GetExecutingAssembly();
+                AssemblyName assemblyName = new AssemblyName(assembly.FullName);
+                var version = assemblyName.Version;
+
+                _client = new RestClient();
+                _client.UserAgent = "maestrano-dotnet/" + version;
+                _client.AddDefaultHeader("Accept", "application/vnd.api+json");
+                _client.AddDefaultHeader("Content-Type", "application/vnd.api+json");
+
+                _client.Authenticator = new HttpBasicAuthenticator(
+                    MnoHelper.With(presetName).Api.Id, 
+                    MnoHelper.With(presetName).Api.Key
+                    );
+
+                _client.BaseUrl = String.Format("{0}{1}", 
+                    MnoHelper.With(presetName).Connec.Host, 
+                    connecScopedPath
+                    );
+                
+            }
+
+            return _client;
         }
 
         /// <summary>
@@ -51,7 +92,7 @@ namespace Maestrano.Connec
         /// </summary>
         /// <param name="groupId">The customer group id</param>
         public static RestClient GetRestClient(string groupId) {
-            return (RestClient)(new Client(groupId))._client;
+            return (RestClient)(new Client(groupId)).InternalClient();
         }
 
         private IRestRequest PrepareRequest(Method method, String path, NameValueCollection parameters, String body)
@@ -77,45 +118,45 @@ namespace Maestrano.Connec
 
         public RestResponse Get(string path)
         {
-            return (RestResponse)_client.Execute(PrepareRequest(Method.GET, path, null, null));
+            return (RestResponse)InternalClient().Execute(PrepareRequest(Method.GET, path, null, null));
         }
 
         public IRestResponse Get(string path, NameValueCollection parameters)
         {
-            return (RestResponse)_client.Execute(PrepareRequest(Method.GET, path, parameters, null));
+            return (RestResponse)InternalClient().Execute(PrepareRequest(Method.GET, path, parameters, null));
         }
 
         public RestResponse<T> Get<T>(string path) where T : new()
         {
-            return (RestResponse<T>)_client.Execute<T>(PrepareRequest(Method.GET, path, null, null));
+            return (RestResponse<T>)InternalClient().Execute<T>(PrepareRequest(Method.GET, path, null, null));
         }
 
         public RestResponse<T> Get<T>(string path, NameValueCollection parameters) where T : new()
         {
-            return (RestResponse<T>)_client.Execute<T>(PrepareRequest(Method.GET, path, parameters, null));
+            return (RestResponse<T>)InternalClient().Execute<T>(PrepareRequest(Method.GET, path, parameters, null));
         }
 
 
 
         public IRestResponse Post(string path, string jsonBody)
         {
-            return (RestResponse)_client.Execute(PrepareRequest(Method.POST, path, null, jsonBody));
+            return (RestResponse)InternalClient().Execute(PrepareRequest(Method.POST, path, null, jsonBody));
         }
 
         public RestResponse<T> Post<T>(string path, string jsonBody) where T : new()
         {
-            return (RestResponse<T>)_client.Execute<T>(PrepareRequest(Method.POST, path, null, jsonBody));
+            return (RestResponse<T>)InternalClient().Execute<T>(PrepareRequest(Method.POST, path, null, jsonBody));
         }
 
 
         public IRestResponse Put(string path, string jsonBody)
         {
-            return (RestResponse)_client.Execute(PrepareRequest(Method.PUT, path, null, jsonBody));
+            return (RestResponse)InternalClient().Execute(PrepareRequest(Method.PUT, path, null, jsonBody));
         }
 
         public RestResponse<T> Put<T>(string path, string jsonBody) where T : new()
         {
-            return (RestResponse<T>)_client.Execute<T>(PrepareRequest(Method.PUT, path, null, jsonBody));
+            return (RestResponse<T>)InternalClient().Execute<T>(PrepareRequest(Method.PUT, path, null, jsonBody));
         }
     }
 }
